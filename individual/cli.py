c3e5fe36f501be78c9d6ac9ae059f1673e7a2a21
Bwalya Mwila <bwalyamwila@Bwalyas-MacBook-Air-4.local>
2025-04-18 17:37:49 -0500

    Added the Week 3 individual Assignment

---
+++ individual/cli.py
@@ -0,0 +1,8 @@
+# Listing animals starting with a letter
+# Bwalya Mwila
+# 04/18/2025
+'''
+ NAME: cli.py - command-line interface exercise 
+SYNOPSIS: python3 cli.py animal by first letter of name 
+DESCRIPTION: Will display a list of all the animals that being with a given first letter from the dataset.
+'''
