9b1f321b59af039ec6fc7b789a722a699c54b4d4
Bwalya Mwila <bwalyamwila@Bwalyas-MacBook-Air-4.local>
2025-04-21 21:47:57 -0500

    Added the end points and other functions and files for the API assignment.

---
+++ individual/api.py
@@ -0,0 +1,37 @@
+import sys
+import argparse
+import flask
+import json
+import csv
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def greeting():
+    return 'Welcome, Curious Animal Lover!'
+
+@app.route('/animalname/<first_letter>')
+def get_animals(first_letter):
+    animal_names = []
+    with open('../data/data.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row:
+                animal_names.append(row[0])
+    names = []
+    first_letter = first_letter.upper()
+    for animal in animal_names:
+        if animal[0] == first_letter:
+            names.append({"animal_name":animal})
+    return json.dumps(names)
+
+@app.route('/help')
+def get_help():
+    return flask.render_template('help.html')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('A sample Flask application/API')
+    parser.add_argument('host', help='the host on which this application is running')
+    parser.add_argument('port', type=int, help='the port on which this application is listening')
+    arguments = parser.parse_args()
+    app.run(host=arguments.host, port=arguments.port, debug=True)
